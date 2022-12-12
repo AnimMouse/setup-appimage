beda0ced5eff4d829c45a53f1e782b7eeb8345d6
AnimMouse <git@animmouse.com>
2022-12-13 01:31:12 +0800

    Initial commit

---
+++ scripts/download.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -eu
+echo ::group::Downloading $name AppImage
+aria2c -o $name.AppImage -x 16 $url
+chmod +x $name.AppImage
+./$name.AppImage --appimage-extract
+mv ./squashfs-root/ ./$name/
+mv ./$name/AppRun ./$name/$name
+rm $name.AppImage
+echo ::endgroup::
