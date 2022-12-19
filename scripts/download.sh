60d197bbafda23b33a9572c8606d76274ec0fe13
AnimMouse <git@animmouse.com>
2022-12-19 19:56:00 +0800

    style: Remove trailing slash

--- scripts/download.sh
+++ scripts/download.sh
@@ -4,7 +4,7 @@ echo ::group::Downloading $name AppImage
 aria2c -o $name.AppImage -x 16 $url
 chmod +x $name.AppImage
 ./$name.AppImage --appimage-extract
-mv ./squashfs-root/ ./$name/
+mv ./squashfs-root ./$name
 mv ./$name/AppRun ./$name/$name
 rm $name.AppImage
 echo ::endgroup::
